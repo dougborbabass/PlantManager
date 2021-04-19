0e703a87845b72300f55f00445768a265bd14449
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-04-19 10:30:40 -0300

    testando propriedade de useState

--- src/components/Buttom.tsx
+++ src/components/Buttom.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from "react-native";
-import colors from '../styles/colors';
+import React from "react";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableOpacityProps,
+} from "react-native";
+import colors from "../styles/colors";
 
-interface ButtonProps {
-    title: string;
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
 }
 
-export function Button({ title } : ButtonProps){
-    return(
-    <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-          <Text style={styles.buttontext}>
-              { title }
-          </Text>
-      </TouchableOpacity>
-    )
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} activeOpacity={0.7} {...rest}>
+      <Text style={styles.buttontext}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 56,
-        paddingHorizontal: 10
-    },
-    buttontext: {
-        color: colors.white,
-        fontSize: 14
-    }
-})
-
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+    paddingHorizontal: 10,
+  },
+  buttontext: {
+    color: colors.white,
+    fontSize: 14,
+  },
+});
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,67 +1,80 @@
-import React from "react";
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 import wateringImg from "../assets/watering.png";
 import { Button } from "../components/Buttom";
 import colors from "../styles/colors";
 
 export function Welcome() {
+  const [visible, setVisible] = useState(false);
+
+  function handleVisibility() {
+    setVisible(true);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
-          Gerencie {'\n'}
-          suas plantas {'\n'}
-          de forma mais fácil
-        </Text>
+        Gerencie {"\n"}
+        suas plantas {"\n"}
+        de forma mais fácil
+      </Text>
 
-      <Image source={wateringImg} style={styles.image}/>
-
+      
+      {
+          visible &&
+          <Image source={wateringImg} style={styles.image} />
+      }
+      
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
-
-      <Button title="Avançar"/>
-
-      
+      <Button title=">" onPress={handleVisibility} />
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: "bold",
-        textAlign: 'center',
-        color: colors.heading,
-        marginTop: 38
-    },
-    subtitle: {
-        textAlign: 'center',
-        fontSize: 18,
-        paddingHorizontal: 20,
-        color: colors.heading
-    },
-    button: {
-        backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 56,
-        width: 56
-    },
-    image: {
-        width: 292,
-        height: 284
-    },
-    buttontext: {
-        color: colors.white,
-        fontSize: 24
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  image: {
+    width: 292,
+    height: 284,
+  },
+  buttontext: {
+    color: colors.white,
+    fontSize: 24,
+  },
+});
