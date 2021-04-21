ba424a57dde7a316fdeb6835a4691f46352b37e0
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-04-21 19:09:51 -0300

    consumindo endpoint da listagem de plantas

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -9,6 +9,7 @@ import {
 
 import { EnvironmentButton } from "../components/EnvironmentButton";
 import { Header } from "../components/Header";
+import { PlantCardPrimary } from "../components/PlantCardPrimary";
 
 import api from "../services/api";
 import colors from "../styles/colors";
@@ -35,52 +36,6 @@ interface PlantProps {
 export function PlantSelect() {
   const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
   const [plants, setPlants] = useState<PlantProps[]>([]);
-  const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
-  const [environmentSelected, setEnvironmentSelected] = useState("all");
-  const [loading, setLoading] = useState(true);
-
-  const [page, setPage] = useState(1);
-  const [loadingMore, setLoadingMore] = useState(false);
-  const [loadedAll, setLoadedAll] = useState(false);
-
-  function handleEnvironmentSelected(environment: string) {
-    setEnvironmentSelected(environment);
-
-    if (environment === "all") return setFilteredPlants(plants);
-
-    const filtered = plants.filter((plant) =>
-      plant.environments.includes(environment)
-    );
-
-    setFilteredPlants(filtered);
-  }
-
-  async function fetchPlants() {
-    const { data } = await api.get(
-      `plants?_sort=name&_order=asc&_page=${page}&_limit=8`
-    );
-
-    if (!data) return setLoading(true);
-
-    if (page > 1) {
-      setPlants((oldValue) => [...oldValue, ...data]);
-      setFilteredPlants((oldValue) => [...oldValue, ...data]);
-    } else {
-      setPlants(data);
-      setFilteredPlants(data);
-    }
-
-    setLoading(false);
-    setLoadingMore(false);
-  }
-
-  function handleFetchMore(distance: number) {
-    if (distance < 1) return;
-
-    setLoadingMore(true);
-    setPage((oldValue) => oldValue + 1);
-    fetchPlants();
-  }
 
   useEffect(() => {
     async function fetchEnvironment() {
@@ -99,8 +54,12 @@ export function PlantSelect() {
   }, []);
 
   useEffect(() => {
+    async function fetchPlants() {
+      const { data } = await api.get('plants');
+      setPlants(data)
+    }
     fetchPlants();
-  }, []);
+  },[])
 
   return (
     <View style={styles.container}>
@@ -117,8 +76,6 @@ export function PlantSelect() {
           renderItem={({ item }) => (
             <EnvironmentButton
               title={item.title}
-              active={item.key === environmentSelected}
-              onPress={() => handleEnvironmentSelected(item.key)}
             />
           )}
           horizontal
@@ -126,6 +83,16 @@ export function PlantSelect() {
           contentContainerStyle={styles.environmentList}
           ListHeaderComponent={<View />}
           ListHeaderComponentStyle={{ marginRight: 32 }}
+        />
+      </View>
+
+      <View style={styles.plants}>
+        <FlatList
+          data={plants}
+          renderItem={( { item }) => (
+            <PlantCardPrimary data={item}/>
+          )}
+          
         />
       </View>
     </View>
