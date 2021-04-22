7c1067bd8508fb0ec8b11630c4727b09f8acc744
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-04-22 10:58:46 -0300

    refactor id das listas

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -119,6 +119,7 @@ export function PlantSelect() {
       <View>
         <FlatList
           data={environments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({ item }) => (
             <EnvironmentButton 
               title={item.title}
@@ -137,6 +138,7 @@ export function PlantSelect() {
       <View style={styles.plants}>
         <FlatList 
           data={filteredPlants}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
             <PlantCardPrimary data={item} />
           )}
