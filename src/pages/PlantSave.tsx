bde757b633318e106255111f076ffd13c1b3011b
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-04-22 13:29:38 -0300

    passando dados da planta selecionada para outra tela

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -10,27 +10,44 @@ import {
   View,
 } from "react-native";
 import { getBottomSpace } from "react-native-iphone-x-helper";
-
 import { SvgFromUri } from "react-native-svg";
+import { useRoute } from "@react-navigation/core";
 
 import waterdrop from "../assets/waterdrop.png";
 import { Button } from "../components/Button";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
+interface Paramns {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+      times: number;
+      repeat_every: string;
+    };
+  };
+}
+
 export function PlantSave() {
+  const route = useRoute();
+  const { plant } = route.params as Paramns;
+
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
-        <SvgFromUri uri="" height={150} width={150} />
+        <SvgFromUri uri={plant.photo} height={150} width={150} />
 
-        <Text style={styles.plantName}>Nome da planta</Text>
+        <Text style={styles.plantName}>
+            {plant.name}
+        </Text>
 
         <Text style={styles.plantAbout}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa nulla
-          dolorem reprehenderit suscipit nostrum corporis iste ad quos non
-          eveniet excepturi praesentium, eius architecto qui? Adipisci dolores
-          ullam saepe nihil!{" "}
+            {plant.about}
         </Text>
       </View>
 
@@ -39,7 +56,7 @@ export function PlantSave() {
           <Image source={waterdrop} style={styles.tipImage} />
 
           <Text style={styles.tipText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            { plant.water_tips }
           </Text>
         </View>
 
@@ -93,8 +110,8 @@ const styles = StyleSheet.create({
     backgroundColor: colors.blue_light,
     padding: 20,
     borderRadius: 20,
-    position: 'relative',
-    bottom: 60
+    position: "relative",
+    bottom: 60,
   },
   tipImage: {
     width: 56,
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -81,7 +81,7 @@ export function PlantSelect() {
   }
 
   function handlePlantSelect(plant: PlantProps){
-    navigation.navigate('PlantSave');
+    navigation.navigate('PlantSave', { plant });
   }
 
   useEffect(() => {
